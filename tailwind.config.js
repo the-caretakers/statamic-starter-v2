05342a9abe71251b1b0f4ecfab1df5a8383f7697
Alex Clark <hello@alexclark.co.nz>
2023-04-19 10:45:29 +0900

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,30 @@ module.exports = {
   content: [
     './resources/**/*.antlers.html',
     './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './content/**/*.md'
+    './resources/**/*.{js,ts,jsx,tsx}',
+    './content/**/*.md',
   ],
   theme: {
-    extend: {},
+    container: {
+        center: true,
+        padding: "1.25rem",
+    },
+    fontFamily: {
+        // Add your fonts here
+        sans: ['Sharp Sans', 'Helvetica', 'arial', 'sans-serif'],
+    },
+    extend: {
+        colors: {
+            // Add your colors here
+        },
+        // Set default transition durations and easing when using the transition utilities.
+        transitionDuration: {
+            DEFAULT: '300ms',
+        },
+        transitionTimingFunction: {
+            DEFAULT: 'cubic-bezier(0.4, 0, 0.2, 1)',
+        },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
