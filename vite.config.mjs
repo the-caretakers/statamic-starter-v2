ce4c29d0860df2429d96f5d51fbc6dc2a8e00fb8
Alex Clark <hello@alexclark.co.nz>
2025-09-07 15:28:18 +1200

    Vite defaults

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,18 +1,13 @@
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { defineConfig, loadEnv } from 'vite';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
-    return {
-        plugins: [
-            laravel([
-                'resources/js/app.js',
-            ]),
-            tailwindcss(),
-        ],
-        server: {
-            open: env.APP_URL
-        }
-    }
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
 });
